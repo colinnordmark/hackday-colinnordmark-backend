cdc50f3970b39a4bae5b3735ab2a5c70f3d0de25
Coliniloc01 <nordmarkcolin@gmail.com>
2023-11-09 07:11:36 +0100

    feat: Adds getAllSongs to service class

--- src/main/java/colin/dev/example/hackdaybackend/service/MusicService.java
+++ src/main/java/colin/dev/example/hackdaybackend/service/MusicService.java
@@ -1,9 +1,12 @@
 package colin.dev.example.hackdaybackend.service;
 
 import colin.dev.example.hackdaybackend.database.music.MusicRepository;
+import colin.dev.example.hackdaybackend.database.music.Song;
 import colin.dev.example.hackdaybackend.database.playlist.PlaylistRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class MusicService {
@@ -12,4 +15,8 @@ public class MusicService {
     PlaylistRepository playlistRepository;
     @Autowired
     MusicRepository musicRepository;
+
+    public List<Song> getAllSongs() {
+        return musicRepository.getAll();
+    }
 }
