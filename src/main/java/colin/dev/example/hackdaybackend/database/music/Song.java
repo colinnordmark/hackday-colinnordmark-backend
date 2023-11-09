dad3333b38d4f5e918a0792f929c701167d3c6ff
Coliniloc01 <nordmarkcolin@gmail.com>
2023-11-09 06:28:06 +0100

    feat: added file for seeding data

--- src/main/java/colin/dev/example/hackdaybackend/database/music/Song.java
+++ src/main/java/colin/dev/example/hackdaybackend/database/music/Song.java
@@ -3,8 +3,10 @@ package colin.dev.example.hackdaybackend.database.music;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "songs")
 public class Song {
     @Id
     int id;
@@ -12,8 +14,6 @@ public class Song {
     String name;
     @Column
     String artist;
-    @Column
-    String source;
     @Column
     String url;
 }
