7fa56a61891f363dcf2487ec9f9966bd0757aaa4
Coliniloc01 <nordmarkcolin@gmail.com>
2023-11-09 07:14:43 +0100

    feat: Adds request mapping for getsongs

--- src/main/java/colin/dev/example/hackdaybackend/web/Controller.java
+++ src/main/java/colin/dev/example/hackdaybackend/web/Controller.java
@@ -1,21 +1,31 @@
 package colin.dev.example.hackdaybackend.web;
 
+import colin.dev.example.hackdaybackend.database.music.Song;
+import colin.dev.example.hackdaybackend.service.MusicService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 
 @RestController
 @RequestMapping("/api")
 public class Controller {
 
-
+    @Autowired
+    MusicService musicService;
 
     @GetMapping
     ResponseEntity<String> helloWorld(){
         return ResponseEntity.ok("Hello world: ");
     }
 
+    @GetMapping("/api/songs")
+    ResponseEntity<List<Song>> getAllSongs(){
+        return ResponseEntity.ok(musicService.getAllSongs());
+    }
 
 }
