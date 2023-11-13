c3e5dda1240a0ec4ab94850abf85274b0fc7c8c9
Coliniloc01 <nordmarkcolin@gmail.com>
2023-11-13 11:15:54 +0100

    Final changes release 1

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,37 @@
 DELETE FROM songs;
 
-INSERT INTO public.songs(id, name, artist, url) VALUES (1, 'Chattermax', 'A bird', 'https://soundcloud.com/bluey-sc/chattermax-feat-joff-bush?si=84397b7e44904aee9d8ac29c4e7ae847&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing');
-INSERT INTO public.songs(id, name, artist, url) VALUES (2, 'Temp2', 'A bird', 'https://soundcloud.com/bluey-sc/chattermax-feat-joff-bush?si=84397b7e44904aee9d8ac29c4e7ae847&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing');
-INSERT INTO public.songs(id, name, artist, url) VALUES (3, 'Temp3', 'A dog', 'https://soundcloud.com/bluey-sc/chattermax-feat-joff-bush?si=84397b7e44904aee9d8ac29c4e7ae847&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing');
-INSERT INTO public.songs(id, name, artist, url) VALUES (4, 'Temp4', 'A human', 'https://soundcloud.com/bluey-sc/chattermax-feat-joff-bush?si=84397b7e44904aee9d8ac29c4e7ae847&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing');
+INSERT INTO public.songs(id, name, artist, url) VALUES (1, 'Så Här', 'Dree Low', 'https://soundcloud.com/dreelow163/sa-har?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing');
+INSERT INTO public.songs(id, name, artist, url) VALUES (2, 'Rush', 'Ayra Starr', 'https://soundcloud.com/ayrastarr/rush?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing');
+INSERT INTO public.songs(id, name, artist, url) VALUES (3, 'Nineb Youk', 'Wine Slow', 'https://soundcloud.com/ninebyouk/wine-slow?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing');
+INSERT INTO public.songs(id, name, artist, url) VALUES (4, 'UNAVAILABLE', 'Davido', 'https://soundcloud.com/davido16056/track-5?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing');
+INSERT INTO public.songs(id, name, artist, url) VALUES (5, 'Water', 'Tyla', 'https://soundcloud.com/tylamusic/water?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing');
+INSERT INTO public.songs(id, name, artist, url) VALUES (6, 'Daylight', 'David Kushner', 'https://soundcloud.com/davidkushner-music/daylight?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing');
+INSERT INTO public.songs(id, name, artist, url) VALUES (7, 'I wanna be yours', 'Silas', 'https://soundcloud.com/user-870885163/i-wanna-be-yours?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing');
+INSERT INTO public.songs(id, name, artist, url) VALUES (8, 'Not Allowed', 'TV Girl', 'https://soundcloud.com/tv-girl/not-allowed?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing');
+INSERT INTO public.songs(id, name, artist, url) VALUES (9, 'Dujem', 'Butrint Imeri', 'https://soundcloud.com/butrintimeri-sc/dujem?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing');
+INSERT INTO public.songs(id, name, artist, url) VALUES (10, 'Say Yes To Heaven', 'Lana Del Rey', 'https://soundcloud.com/lana-del-rey/say-yes-to-heaven?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing');
+
+INSERT INTO public.songs(id, name, artist, url) VALUES (11, 'Set Fire to the Rain', 'Adele', 'https://music.youtube.com/watch?v=a2giXO6eyuI&feature=shared');
+INSERT INTO public.songs(id, name, artist, url) VALUES (12, 'Billie Jean', 'Michael Jackson', 'https://music.youtube.com/watch?v=Kr4EQDVETuA&feature=shared');
+INSERT INTO public.songs(id, name, artist, url) VALUES (13, 'Wildest Dreams', 'Taylor Swift', 'https://music.youtube.com/watch?v=bhnaPBK9nOQ&feature=shared');
+INSERT INTO public.songs(id, name, artist, url) VALUES (14, 'Blinding Lights', 'The Weeknd', 'https://music.youtube.com/watch?v=H64a2ggVIWc&feature=shared');
+INSERT INTO public.songs(id, name, artist, url) VALUES (15, 'Territory', 'The Blaze', 'https://music.youtube.com/watch?v=54fea7wuV6s&feature=shared');
+INSERT INTO public.songs(id, name, artist, url) VALUES (16, 'Efteraar', 'Mark Jubel', 'https://music.youtube.com/watch?v=dzYjelWXKMA&feature=shared');
+INSERT INTO public.songs(id, name, artist, url) VALUES (17, 'Sapienthia', 'Luna', 'https://music.youtube.com/watch?v=-0rS24e1ScY&feature=shared');
+INSERT INTO public.songs(id, name, artist, url) VALUES (18, 'In Pieces', 'Don Carlos', 'https://music.youtube.com/watch?v=Zxw_feRPmWQ&feature=shared');
+INSERT INTO public.songs(id, name, artist, url) VALUES (19, 'Stans Mood', 'Stan Getz', 'https://music.youtube.com/watch?v=QV_ldmz3KT0&feature=shared');
+INSERT INTO public.songs(id, name, artist, url) VALUES (20, 'Berdiri Bulu Romaku', 'Hetty Koes Endang', 'https://music.youtube.com/watch?v=_WOumm1JcKo&feature=shared');
+
+INSERT INTO public.songs(id, name, artist, url) VALUES
+                                                    (30, 'Shape of You', 'Ed Sheeran', 'https://open.spotify.com/track/7qiZfU4dY1lWllzX7mPBI3?si=some_id'),
+                                                    (31, 'Bad Guy', 'Billie Eilish', 'https://open.spotify.com/track/2Fxmhks0bxGSBdJ92vM42m?si=some_other_id'),
+                                                    (33, 'Senorita', 'Shawn Mendes, Camila Cabello', 'https://open.spotify.com/track/some_unique_id_23'),
+                                                    (34, 'Sunflower', 'Post Malone, Swae Lee', 'https://open.spotify.com/track/some_unique_id_24'),
+                                                    (35, 'Dance Monkey', 'Tones And I', 'https://open.spotify.com/track/some_unique_id_25'),
+                                                    (36, 'Someone You Loved', 'Lewis Capaldi', 'https://open.spotify.com/track/some_unique_id_26'),
+                                                    (37, 'Watermelon Sugar', 'Harry Styles', 'https://open.spotify.com/track/some_unique_id_27'),
+                                                    (38, 'Memories', 'Maroon 5', 'https://open.spotify.com/track/some_unique_id_28');
+
 
 INSERT INTO public.playlist(id) VALUES (1);
 INSERT INTO public.playlist(id) VALUES (2);
